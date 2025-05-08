1534d012b179556917ecea06b0208369406a7816
sameera-g-mathad <sameeragmathad@gmail.com>
2025-05-08 13:02:06 -0500

    created super class to handle hydra config

---
+++ team_ops/hydra_config.py
@@ -0,0 +1,21 @@
+import logging
+from hydra import initialize, compose
+
+
+class HConfig:
+    """
+    Experimental
+    """
+
+    def __init__(self, conf_path: str = "conf", conf_file: str = "config"):
+        # Setting up basic configuration for logging
+        logging.basicConfig(
+            level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+        )
+
+        self.log = logging.getLogger(__name__)  # Initializing a logger instance
+
+        # Setting a path that holds all the config files
+        with initialize(config_path=conf_path, version_base=None):
+            # Reading and loading configuration into 'cfg' instance variable.
+            self._cfg = compose(config_name=conf_file)
