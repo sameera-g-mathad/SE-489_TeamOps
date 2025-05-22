d739158049bd87b1abbfc3f2ecbae32d1398da44
sameera-g-mathad <sameeragmathad@gmail.com>
2025-05-22 17:19:18 -0500

    added support for prometheus

--- team_ops/predict_model.py
+++ team_ops/predict_model.py
@@ -1,7 +1,14 @@
+from prometheus_client import start_http_server, Summary, Counter
 from team_ops import Model
 
+# Define a metric to track time spent and requests made
+REQUEST_TIME = Summary("request_processing_seconds", "Time spent processing request")
+REQUEST_COUNT = Counter("request_count", "Total number of requests")
+REQUEST_ERROR = Counter("request_error", "Total number of errors")
 
-def main():
+
+@REQUEST_TIME.time()
+def inference():
     """
     Main function to train the model.
     """
@@ -12,15 +19,23 @@ def main():
     model.load_model()
 
     while True:
-        prompt = input("Enter the prompt (Type `exit` to quit!!): ")
-        if prompt.lower() == "exit":
-            break
-        prediction = model.predict(prompt)
+        try:
+            prompt = input("Enter the prompt (Type `exit` to quit!!): ")
+            if prompt.lower() == "exit":
+                break
+            prediction = model.predict(prompt)
 
-        print("--------------------")
-        print(f"{prediction}")
-        print("--------------------")
+            print("--------------------")
+            print(f"{prediction}")
+            print("--------------------")
+            REQUEST_COUNT.inc()  # Increment the request count
+
+        except Exception as e:
+            REQUEST_ERROR.inc()  # Increment the error count
+            raise e
 
 
 if __name__ == "__main__":
-    main()
+    start_http_server(8000)
+    # Start the inference
+    inference()
